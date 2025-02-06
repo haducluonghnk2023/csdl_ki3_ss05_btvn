bee124fdcf5ee57de8583e314eb15a983e5c6838
haducluonghnk2023 <147390439+haducluonghnk2023@users.noreply.github.com>
2025-02-06 10:05:29 +0700

    Update bai7.sql

--- bai7.sql
+++ bai7.sql
@@ -14,15 +14,9 @@ LEFT JOIN students s ON e.student_id = s.student_id
 WHERE c.fee > 1000000 OR e.enrollment_date IS NULL
 ORDER BY c.fee DESC, c.course_name ASC;
 -- 4
-SELECT s.name,s.email,c.course_name,e.enrollment_date
-FROM students s
-LEFT JOIN enrollments e ON s.student_id = e.student_id
-LEFT JOIN courses c ON e.course_id = c.course_id
-WHERE s.email IS NULL OR c.fee > 1000000
-UNION
-SELECT s.name,s.email,c.course_name,e.enrollment_date
-FROM courses c
-LEFT JOIN enrollments e ON c.course_id = e.course_id
-LEFT JOIN students s ON e.student_id = s.student_id
-WHERE s.email IS NULL OR c.fee > 1000000
-ORDER BY name ASC, course_name ASC;
+select s.name as student_name, s.email, c.course_name, e.enrollment_date
+from students s
+right join enrollments e on s.student_id = e.student_id
+right join courses c on e.course_id = c.course_id
+where (s.email is null or c.fee > 1000000)
+ORDER BY s.name asc, c.course_name asc;
